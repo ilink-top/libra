3557fc9fc47d1877cdab03b3af138e4eeeb41303
tao <xht0826@163.com>
2020-04-24 16:51:03 +0800

    debug

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -9,13 +9,13 @@ class IndexController extends BaseController
     public function index()
     {
         return view('index.index', [
-            'lastLoginAdmins' => Admin::orderBy('logined_at', 'desc')->limit(8)->get(),
             'systemInfo' => [
                 'os' => PHP_OS,
                 'php_version' => PHP_VERSION,
                 'upload_max_filesize' => get_cfg_var('upload_max_filesize') ? get_cfg_var('upload_max_filesize') : '不允许上传附件',
                 'max_execution_time' => get_cfg_var("max_execution_time"),
             ],
+            'lastLoginAdmins' => Admin::orderBy('logined_at', 'desc')->limit(8)->get(),
         ]);
     }
 }
